ae91e9a72719864b0614a95817f8f42b189b8ee1
leekt <leekt216@gmail.com>
2023-05-28 20:54:19 +0900

    deploy script

--- scripts/DeployKernel.s.sol
+++ scripts/DeployKernel.s.sol
@@ -9,10 +9,8 @@ contract DeployKernel is Script {
         uint256 key = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(key);
         KernelFactory factory = new KernelFactory(IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
-        factory.addStake{value:1}(1);
         ECDSAValidator validator = new ECDSAValidator();
         ECDSAKernelFactory ecdsaFactory = new ECDSAKernelFactory(factory, validator, IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789));
-        ecdsaFactory.addStake{value:1}(1);
         vm.stopBroadcast();
     }
 }
--- tasks/test_userOp.ts
+++ tasks/test_userOp.ts
@@ -10,7 +10,7 @@ task("test-userop", "deploy erc20 paymaster")
       const addr = await signer.getAddress();
       console.log("signer address: ", addr);
       const entrypoint = await hre.ethers.getContractAt("EntryPoint", "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789");
-      const ecdsaFactory = await hre.ethers.getContractAt("ECDSAKernelFactory", "0x6b337f4ddb17b4ba93bcde99661ab46aec1d2823");
+      const ecdsaFactory = await hre.ethers.getContractAt("ECDSAKernelFactory", "0x08e627ca6a0593c807091726a7fbb2887a1cb556");
       const account = await ecdsaFactory.getAccountAddress(addr, 0);
       const kernel = await hre.ethers.getContractAt("Kernel", account);
       console.log("maxFeePerGas : ", await hre.ethers.provider.getGasPrice());
@@ -50,7 +50,7 @@ task("create-kernel", "create kernel")
       const addr = await signer.getAddress();
       console.log("signer address: ", addr);
       const entrypoint = await hre.ethers.getContractAt("EntryPoint", "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789");
-      const ecdsaFactory = await hre.ethers.getContractAt("ECDSAKernelFactory", "0x6b337f4ddb17b4ba93bcde99661ab46aec1d2823");
+      const ecdsaFactory = await hre.ethers.getContractAt("ECDSAKernelFactory", "0x08e627ca6a0593c807091726a7fbb2887a1cb556");
       const account = await ecdsaFactory.getAccountAddress(addr, kernel_id);
       await ecdsaFactory.createAccount(addr, kernel_id);
       const kernel = await hre.ethers.getContractAt("Kernel", account);
