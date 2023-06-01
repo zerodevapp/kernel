14c47be3d0445c248a32dfd776416aa589313396
leekt <leekt216@gmail.com>
2023-06-01 15:40:34 +0900

    using simple trick to adapt the storage access rule

--- src/validator/ERC165SessionKeyValidator.sol
+++ src/validator/ERC165SessionKeyValidator.sol
@@ -8,7 +8,7 @@ import "src/utils/KernelHelper.sol";
 
 // idea, we can make this merkle root
 struct ERC165SessionKeyStorage {
-    address key;
+    bool enabled;
     bytes4 selector;
     bytes4 interfaceId;
     uint48 validUntil;
@@ -17,7 +17,7 @@ struct ERC165SessionKeyStorage {
 }
 
 contract ERC165SessionKeyValidator is IKernelValidator {
-    mapping(address => ERC165SessionKeyStorage) public sessionKeys;
+    mapping(address sessionKey => mapping(address kernel => ERC165SessionKeyStorage)) public sessionKeys;
 
     function enable(bytes calldata _data) external {
         address sessionKey = address(bytes20(_data[0:20]));
@@ -26,12 +26,14 @@ contract ERC165SessionKeyValidator is IKernelValidator {
         uint48 validUntil = uint48(bytes6(_data[28:34]));
         uint48 validAfter = uint48(bytes6(_data[34:40]));
         uint32 addressOffset = uint32(bytes4(_data[40:44]));
-        sessionKeys[msg.sender] =
-            ERC165SessionKeyStorage(sessionKey, selector, interfaceId, validUntil, validAfter, addressOffset);
+        sessionKeys[sessionKey][msg.sender] =
+            ERC165SessionKeyStorage(true, selector, interfaceId, validUntil, validAfter, addressOffset);
     }
 
-    function disable(bytes calldata) external {
-        delete sessionKeys[msg.sender];
+    function disable(bytes calldata _data) external {
+        address sessionKey = address(bytes20(_data[0:20]));
+
+        delete sessionKeys[sessionKey][msg.sender];
     }
 
     function validateSignature(bytes32, bytes calldata) external pure override returns (uint256) {
@@ -45,8 +47,8 @@ contract ERC165SessionKeyValidator is IKernelValidator {
     {
         bytes32 hash = ECDSA.toEthSignedMessageHash(_userOpHash);
         address recovered = ECDSA.recover(hash, _userOp.signature);
-        ERC165SessionKeyStorage storage sessionKey = sessionKeys[_userOp.sender];
-        if (recovered != sessionKey.key) {
+        ERC165SessionKeyStorage storage sessionKey = sessionKeys[recovered][_userOp.sender];
+        if (!sessionKey.enabled) {
             return SIG_VALIDATION_FAILED;
         }
         require(bytes4(_userOp.callData[0:4]) == sessionKey.selector, "not supported selector");
