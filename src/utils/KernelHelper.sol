1fc5309c70e902885b43fc2d92df151b7d775474
leekt <leekt216@gmail.com>
2023-06-10 00:37:38 +0900

    fix : kernel helper

--- src/utils/KernelHelper.sol
+++ src/utils/KernelHelper.sol
@@ -7,12 +7,18 @@ function _intersectValidationData(uint256 a, uint256 b) pure returns (uint256 va
     if (uint160(a) != uint160(b)) {
         return SIG_VALIDATION_FAILED;
     }
-    uint48 validAfterA = uint48(a >> 160);
-    uint48 validUntilA = uint48(a >> (48 + 160));
-    uint48 validAfterB = uint48(b >> 160);
-    uint48 validUntilB = uint48(b >> (48 + 160));
+    uint48 validAfterA = uint48(a >> (160 + 48));
+    uint48 validUntilA = uint48(a >> 160);
+    if(validUntilA == 0) {
+        validUntilA = type(uint48).max;
+    }
+    uint48 validAfterB = uint48(b >> (160 + 48));
+    uint48 validUntilB = uint48(b >> 160);
+    if(validUntilB == 0) {
+        validUntilB = type(uint48).max;
+    }
 
     if (validAfterA < validAfterB) validAfterA = validAfterB;
     if (validUntilA > validUntilB) validUntilA = validUntilB;
-    validationData = uint256(uint160(a)) | (uint256(validAfterA) << 160) | (uint256(validUntilA) << (48 + 160));
+    validationData = uint256(uint160(a)) | (uint256(validUntilA) << 160) | (uint256(validAfterA) << (48 + 160));
 }
