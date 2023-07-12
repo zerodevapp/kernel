ccc01a6705cdcfe1ed78834a6ff7b0cf36404d18
taek <leekt216@gmail.com>
2023-07-13 00:23:23 +0900

    fix: return value on temp kernel (#13)

--- src/factory/TempKernel.sol
+++ src/factory/TempKernel.sol
@@ -176,9 +176,29 @@ contract TempKernel is EIP712, IAccount {
         if (address(validator) != address(0)) {
             validator.enable(getStorage().validatorData);
         }
+        
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
 
-        (bool success, bytes memory ret) = _newImplementation.delegatecall(msg.data);
-        require(success, string(ret));
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), _newImplementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
     }
 
     // WARTNING: this function is NOT VIEW
@@ -223,6 +243,8 @@ contract TempKernel is EIP712, IAccount {
             ret := ptr
             success := result
         }
-        require(success, string(ret));
+        if(!success) {
+            revert(string(ret));
+        }
     }
 }
