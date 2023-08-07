ade2d14bb67097eb9ef79aa1bd7f854fdd76ba4c
leekt <leekt216@gmail.com>
2023-08-07 15:12:25 +0900

    remove kernel lite gas report

--- gas/gas_report.sh
+++ gas/gas_report.sh
@@ -1,3 +1,2 @@
 forge build
 forge test --gas-report --match-path test/foundry/Kernel.t.sol > gas/ecdsa/report.txt
-forge test --gas-report --match-path test/foundry/KernelLite.t.sol > gas/ecdsa/report-lite.txt
