2f1a8cd1f887f56db97e68f78ee5407778cc8d7e
leekt <leekt216@gmail.com>
2023-08-16 00:09:16 +0900

    deployed on mumbai

--- scripts/DeployKernel.s.sol
+++ scripts/DeployKernel.s.sol
@@ -7,34 +7,26 @@ import "forge-std/console.sol";
 contract DeployKernel is Script {
     address constant DEPLOYER = 0x9775137314fE595c943712B0b336327dfa80aE8A;
     address constant ENTRYPOINT_0_6 = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
-    address payable constant EXPECTED_KERNEL_ADDRESS = payable(0xf048AD83CB2dfd6037A43902a2A5Be04e53cd2Eb);
-    address payable constant EXPECTED_KERNEL_FACTORY_ADDRESS = payable(0x5de4839a76cf55d0c90e2061ef4386d962E15ae3);
+    address payable constant EXPECTED_KERNEL_ADDRESS = payable(0xA87e80ed52992e4FB0809fc0B1c0629CF6d43F17);
+    address payable constant EXPECTED_KERNEL_FACTORY_ADDRESS = payable(0xeBf88f6f78b3aBD60744699c06e97415d2fE15Bb);
     function run() public {
         uint256 key = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        Kernel kernel = new Kernel(IEntryPoint(ENTRYPOINT_0_6));
+        bytes memory code = address(kernel).code;
         vm.startBroadcast(key);
-        Kernel kernel;
-        if(EXPECTED_KERNEL_ADDRESS.code.length == 0 ){
-            kernel = new Kernel{salt:0}(IEntryPoint(ENTRYPOINT_0_6));
-            console.log("Kernel address: %s", address(kernel));
-        } else {
-            kernel = Kernel(EXPECTED_KERNEL_ADDRESS);
-        }
         KernelFactory factory;
         if(EXPECTED_KERNEL_FACTORY_ADDRESS.code.length == 0){
-            factory = new KernelFactory{salt:0}(DEPLOYER, IEntryPoint(ENTRYPOINT_0_6));
-            console.log("KernelFactory address: %s", address(factory));
+          factory = new KernelFactory{salt:0}(DEPLOYER);
+          console.log("KernelFactory address: %s", address(factory));
         } else {
             factory = KernelFactory(EXPECTED_KERNEL_FACTORY_ADDRESS);
         }
-        if(factory.isAllowedImplementation(address(kernel)) == false) {
-            console.log("Registering kernel implementation");
-            factory.setImplementation(address(kernel), true);
-        }
-        IEntryPoint entryPoint = IEntryPoint(ENTRYPOINT_0_6);
-        IStakeManager.DepositInfo memory info = entryPoint.getDepositInfo(address(factory));
-        if(info.stake == 0) {
-            console.log("Staking 1 wei to factory");
-            factory.addStake{value:1}(1);
+        address expected_addr = factory.getImplementationAddress(keccak256("v2.1"));
+        if(expected_addr.code.length == 0 ) {
+            kernel = Kernel(payable(factory.deployImplementation(keccak256("v2.1"), code)));
+            console.log("Kernel address: %s", address(kernel));
+        } else {
+            console.log("already deployed on %s", expected_addr);
         }
         vm.stopBroadcast();
     }
--- src/factory/KernelFactory.sol
+++ src/factory/KernelFactory.sol
@@ -12,24 +12,36 @@ import "src/utils/Create2Proxy.sol";
 
 contract KernelFactory is AdminLessERC1967Factory, Ownable {
     error InvalidImplementation();
+    event Implementation(bytes32 indexed tagHash, address indexed implementation); 
+
+    bytes32 immutable public create2CodeHash;
     bytes public deployByteCode;
+    IEntryPoint public entryPoint;
 
     constructor(address _owner) {
         _initializeOwner(_owner);
+        create2CodeHash = keccak256(type(Create2Proxy).creationCode);
     }
 
     function getImplementationAddress(bytes32 _tagHash) public view returns (address predicted) {
-        bytes memory code = type(Create2Proxy).creationCode;
+        bytes32 codeHash = create2CodeHash;
         bytes32 hash = keccak256(
-            abi.encodePacked(bytes1(0xff), address(this), _tagHash, keccak256(code))
+            abi.encodePacked(bytes1(0xff), address(this), _tagHash, codeHash)
         );
         // NOTE: cast last 20 bytes of hash to address
         return address(uint160(uint(hash)));
     }
 
-    function deployImplementation(bytes32 tagHash, bytes memory bytecode) external onlyOwner returns(address) {
+    function deployImplementation(bytes32 tagHash, bytes memory bytecode) external onlyOwner returns(address impl) {
         deployByteCode = bytecode;
-        return address(new Create2Proxy{salt: tagHash}());
+        impl = address(new Create2Proxy{salt: tagHash}());
+        delete deployByteCode;
+        emit Implementation(tagHash, impl);
+    }
+
+    function setEntryPoint(IEntryPoint _entryPoint) external onlyOwner {
+        // not setting this on constructor since we don't need stake right now
+        entryPoint = _entryPoint;
     }
 
     function createAccount(bytes32 _tagHash, bytes calldata _data, uint256 _index)
@@ -48,5 +60,19 @@ contract KernelFactory is AdminLessERC1967Factory, Ownable {
     function getAccountAddress(bytes calldata _data, uint256 _index) public view returns (address) {
         bytes32 salt = bytes32(uint256(keccak256(abi.encodePacked(_data, _index))) & type(uint96).max);
         return predictDeterministicAddress(salt);
+    }
+
+    // in case we need to stake
+    // stake functions
+    function addStake(uint32 unstakeDelaySec) external payable onlyOwner {
+        entryPoint.addStake{value: msg.value}(unstakeDelaySec);
+    }
+
+    function unlockStake() external onlyOwner {
+        entryPoint.unlockStake();
+    }
+
+    function withdrawStake(address payable withdrawAddress) external onlyOwner {
+        entryPoint.withdrawStake(withdrawAddress);
     }
 }
