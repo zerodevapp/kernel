8d576d8759528453439667bbfffcdb79f3dbdf0e
leekt <leekt216@gmail.com>
2023-08-22 18:54:19 +0900

    gas report

--- gas/gas_report.sh
+++ gas/gas_report.sh
@@ -1,2 +1,3 @@
 forge build
-forge test --gas-report --match-path test/foundry/Kernel.t.sol > gas/ecdsa/report.txt
+forge test --gas-report --match-path test/foundry/KernelECDSA.t.sol > gas/ecdsa/report.txt
+forge test --gas-report --match-path test/foundry/KernelLiteECDSA.t.sol > gas/ecdsa/report-lite.txt
