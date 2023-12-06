1a277d87683db712adc697600edbb7fd20f11930
David Eiber <davideiber@gmail.com>
2023-12-06 00:19:02 -0500

    Add p256 validator (#48)
    * Dev (#47)
    
    * Added new ERC1271 signature verification logic in Kernel v0.2.3 (#43)
    
    * Added 1271 wrapper
    
    * Update kernel version to 0.2.3
    
    * use kernel name and version from constants in tests
    
    * added delegatecall support (#44)
    
    * session key validator fixed for batch scenario
    
    * test: fuzz testing for batched options include array
    
    * fix: warning removed, forge fmt (#46)
    
    ---------
    
    Co-authored-by: David Eiber <davideiber@gmail.com>
    
    * Initial commit
    
    * Update P256Validator.t.sol
    
    * forge install: FreshCryptoLib
    
    * Refactor with helper functions
    
    * Add P256Validator, tests, deps
    
    * Remove py scripts used for initial testing
    
    * Update foundry.toml
    
    * update deps
    
    * add non-malleable signatures
    
    ---------
    
    Co-authored-by: taek <leekt216@gmail.com>

---
+++ src/validator/P256Validator.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {UserOperation} from "I4337/interfaces/UserOperation.sol";
+import {ECDSA} from "solady/utils/ECDSA.sol";
+import {IKernelValidator} from "../interfaces/IKernelValidator.sol";
+import {ValidationData} from "../common/Types.sol";
+import {SIG_VALIDATION_FAILED} from "../common/Constants.sol";
+import {P256} from "p256-verifier/P256.sol";
+
+contract P256Validator is IKernelValidator {
+    event P256PublicKeysChanged(address indexed kernel, PublicKey indexed oldKeys, PublicKey indexed newKeys);
+
+    struct PublicKey {
+        uint256 x;
+        uint256 y;
+    }
+
+    error BadKey();
+
+    mapping(address => PublicKey) public p256PublicKey;
+
+    function enable(bytes calldata _data) external payable override {
+        PublicKey memory key = abi.decode(_data, (PublicKey));
+        if (key.x == 0 || key.y == 0) {
+            revert BadKey();
+        }
+        PublicKey memory oldKey = p256PublicKey[msg.sender];
+        p256PublicKey[msg.sender] = key;
+        emit P256PublicKeysChanged(msg.sender, oldKey, key);
+    }
+
+    function disable(bytes calldata) external payable override {
+        delete p256PublicKey[msg.sender];
+    }
+
+    function validateUserOp(UserOperation calldata _userOp, bytes32 _userOpHash, uint256) external payable override returns (ValidationData validationData) {
+        (uint256 r, uint256 s) = abi.decode(_userOp.signature, (uint256, uint256));
+        PublicKey memory key = p256PublicKey[_userOp.sender];
+        bytes32 hash = ECDSA.toEthSignedMessageHash(_userOpHash);
+        if (P256.verifySignature(hash, r, s, key.x, key.y)) {
+            return ValidationData.wrap(0);
+        } 
+        if (!P256.verifySignature(_userOpHash, r, s, key.x, key.y)) {
+            return SIG_VALIDATION_FAILED;
+        }
+    }
+
+    function validateSignature(bytes32 hash, bytes calldata signature) external view override returns (ValidationData) {
+        (uint256 r, uint256 s) = abi.decode(signature, (uint256, uint256));
+        PublicKey memory key = p256PublicKey[msg.sender];
+        if (P256.verifySignature(hash, r, s, key.x, key.y)) {
+            return ValidationData.wrap(0);
+        }
+        bytes32 ethHash = ECDSA.toEthSignedMessageHash(hash);
+        if (!P256.verifySignature(ethHash, r, s, key.x, key.y)) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return ValidationData.wrap(0);
+    }
+
+    function validCaller(address _caller, bytes calldata) external view override returns (bool) {
+        revert NotImplemented();
+    }
+}
--- test/foundry/KernelLiteECDSA.t.sol
+++ test/foundry/KernelLiteECDSA.t.sol
@@ -57,16 +57,12 @@ contract KernelECDSATest is KernelTestBase {
     function test_set_default_validator() external override {
         TestValidator newValidator = new TestValidator();
         bytes memory empty;
-        UserOperation memory op = entryPoint.fillUserOp(
-            address(kernel),
+        UserOperation memory op = buildUserOperation(
             abi.encodeWithSelector(KernelStorage.setDefaultValidator.selector, address(newValidator), empty)
         );
-        op.signature = signUserOp(op);
-        UserOperation[] memory ops = new UserOperation[](1);
-        ops[0] = op;
         vm.expectEmit(true, true, true, false, address(entryPoint));
         emit UserOperationEvent(entryPoint.getUserOpHash(op), address(kernel), address(0), op.nonce, false, 0, 0);
-        entryPoint.handleOps(ops, beneficiary);
+        performUserOperationWithSig(op);
     }
 
     function signUserOp(UserOperation memory op) internal view override returns (bytes memory) {
@@ -101,14 +97,9 @@ contract KernelECDSATest is KernelTestBase {
 
     function test_transfer_ownership() external {
         address newOwner = makeAddr("new owner");
-        UserOperation memory op = entryPoint.fillUserOp(
-            address(kernel), abi.encodeWithSelector(KernelLiteECDSA.transferOwnership.selector, newOwner)
-        );
-        op.signature = signUserOp(op);
-        UserOperation[] memory ops = new UserOperation[](1);
-        ops[0] = op;
+        UserOperation memory op = buildUserOperation(abi.encodeWithSelector(KernelLiteECDSA.transferOwnership.selector, newOwner));
         vm.expectEmit(true, true, true, false, address(entryPoint));
         emit UserOperationEvent(entryPoint.getUserOpHash(op), address(kernel), address(0), op.nonce, false, 0, 0);
-        entryPoint.handleOps(ops, beneficiary);
+        performUserOperationWithSig(op);
     }
 }
