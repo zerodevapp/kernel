fa3d33994da98d4c807f25b20f88cb2b2ea9987d
leekt <leekt216@gmail.com>
2023-12-11 21:56:15 +0900

    ZeroDev-RW-4 and ZeroDev-RW-2 fix

--- src/validator/WeightedECDSAValidator.sol
+++ src/validator/WeightedECDSAValidator.sol
@@ -49,7 +49,7 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
         voteStatus;
 
     function _domainNameAndVersion() internal pure override returns (string memory, string memory) {
-        return ("WeightedECDSAValidator", "1");
+        return ("WeightedECDSAValidator", "0.0.1");
     }
 
     function enable(bytes calldata _data) external payable override {
@@ -83,6 +83,31 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
         delete weightedStorage[msg.sender];
     }
 
+    function renew(address[] calldata _guardians, uint24[] calldata _weights, uint24 _threshold, uint48 _delay) external payable {
+        require(weightedStorage[msg.sender].totalWeight != 0, "Not enabled");
+        address currentGuardian = weightedStorage[msg.sender].firstGuardian;
+        while (currentGuardian != msg.sender) {
+            address nextGuardian = guardian[currentGuardian][msg.sender].nextGuardian;
+            delete guardian[currentGuardian][msg.sender];
+            currentGuardian = nextGuardian;
+        }
+        delete weightedStorage[msg.sender];
+        require(_guardians.length == _weights.length, "Length mismatch");
+        weightedStorage[msg.sender].firstGuardian = msg.sender;
+        for (uint256 i = 0; i < _guardians.length; i++) {
+            require(_guardians[i] != msg.sender, "Guardian cannot be self");
+            require(_guardians[i] != address(0), "Guardian cannot be 0");
+            require(_weights[i] != 0, "Weight cannot be 0");
+            require(guardian[_guardians[i]][msg.sender].weight == 0, "Guardian already enabled");
+            guardian[_guardians[i]][msg.sender] =
+                GuardianStorage({weight: _weights[i], nextGuardian: weightedStorage[msg.sender].firstGuardian});
+            weightedStorage[msg.sender].firstGuardian = _guardians[i];
+            weightedStorage[msg.sender].totalWeight += _weights[i];
+        }
+        weightedStorage[msg.sender].delay = _delay;
+        weightedStorage[msg.sender].threshold = _threshold;
+    }
+
     function approve(bytes32 _callDataAndNonceHash, address _kernel) external {
         require(guardian[msg.sender][_kernel].weight != 0, "Guardian not enabled");
         require(weightedStorage[_kernel].threshold != 0, "Kernel not enabled");
@@ -91,6 +116,11 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
         VoteStorage storage vote = voteStatus[_callDataAndNonceHash][msg.sender][_kernel];
         require(vote.status == VoteStatus.NA, "Already voted");
         vote.status = VoteStatus.Approved;
+        (, bool isApproved) = getApproval(_kernel, _callDataAndNonceHash);
+        if (isApproved) {
+            proposal.status = ProposalStatus.Approved;
+            proposal.validAfter = ValidAfter.wrap(uint48(block.timestamp + weightedStorage[_kernel].delay));
+        }
     }
 
     function approveWithSig(bytes32 _callDataAndNonceHash, address _kernel, bytes calldata sigs) external {
@@ -108,6 +138,12 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
             VoteStorage storage vote = voteStatus[_callDataAndNonceHash][signer][_kernel];
             require(vote.status == VoteStatus.NA, "Already voted");
             vote.status = VoteStatus.Approved;
+        }
+
+        (, bool isApproved) = getApproval(_kernel, _callDataAndNonceHash);
+        if (isApproved) {
+            proposal.status = ProposalStatus.Approved;
+            proposal.validAfter = ValidAfter.wrap(uint48(block.timestamp + weightedStorage[_kernel].delay));
         }
     }
 
@@ -131,7 +167,9 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
         if (strg.threshold == 0) {
             return SIG_VALIDATION_FAILED;
         }
-        if (proposal.status == ProposalStatus.Ongoing) {
+        (uint256 totalWeight, bool passed) = getApproval(msg.sender, callDataAndNonceHash);
+        uint256 threshold = strg.threshold;
+        if (proposal.status == ProposalStatus.Ongoing && !passed) {
             if (strg.delay != 0) {
                 // if delay > 0, only allow proposal to be approved before execution
                 return SIG_VALIDATION_FAILED;
@@ -139,10 +177,9 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
             bytes calldata sig = userOp.signature;
             // parse sig with 65 bytes
             uint256 sigCount = sig.length / 65;
-            (uint256 totalWeight, bool passed) = getApproval(msg.sender, callDataAndNonceHash);
             address signer;
             VoteStorage storage vote;
-            for (uint256 i = 0; i < sigCount; i++) {
+            for (uint256 i = 0; i < sigCount && !passed; i++) {
                 // last sig is for userOpHash verification
                 signer = ECDSA.recover(
                     _hashTypedData(
@@ -156,14 +193,17 @@ contract WeightedECDSAValidator is EIP712, IKernelValidator {
                 } // skip if already voted
                 vote.status = VoteStatus.Approved;
                 totalWeight += guardian[signer][msg.sender].weight;
+                if (totalWeight >= threshold) {
+                    passed = true;
+                }
             }
-            if (totalWeight >= strg.threshold) {
+            if (passed) {
                 validationData = packValidationData(ValidAfter.wrap(0), ValidUntil.wrap(0));
                 proposal.status = ProposalStatus.Executed;
             } else {
                 validationData = SIG_VALIDATION_FAILED;
             }
-        } else if (proposal.status == ProposalStatus.Approved) {
+        } else if (proposal.status == ProposalStatus.Approved || passed) {
             validationData = packValidationData(proposal.validAfter, ValidUntil.wrap(0));
             proposal.status = ProposalStatus.Executed;
         } else {
