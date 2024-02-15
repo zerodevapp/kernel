b5ca36fc55966ddd4eea271d54b797e1387179c8
Sahil Vasava <sahilvasava@gmail.com>
2024-02-15 19:14:12 +0700

    fix: added validator in the storage mapping of SignaturePolicy to fix unauthenticated state change vuln

--- src/validator/modularPermission/policies/SignaturePolicy.sol
+++ src/validator/modularPermission/policies/SignaturePolicy.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.8.0;
 import "../IPolicy.sol";
 
 contract SignaturePolicy is IPolicy {
-    mapping(bytes32 => mapping(address => mapping(address => bool))) public allowedRequestor;
+    mapping(
+        address permissionValidator
+            => mapping(bytes32 permissionId => mapping(address requestor => mapping(address kernel => bool)))
+    ) public allowedRequestor;
 
     function registerPolicy(address kernel, bytes32 permissionId, bytes calldata policyData) external payable {
         address[] memory callers = abi.decode(policyData, (address[]));
         for (uint256 i = 0; i < callers.length; i++) {
             if (callers[i] == address(0)) {
-                allowedRequestor[permissionId][kernel][kernel] = true;
+                allowedRequestor[msg.sender][permissionId][kernel][kernel] = true;
             } else {
-                allowedRequestor[permissionId][callers[i]][kernel] = true;
+                allowedRequestor[msg.sender][permissionId][callers[i]][kernel] = true;
             }
         }
     }
@@ -34,7 +37,7 @@ contract SignaturePolicy is IPolicy {
         bytes32 rawHash,
         bytes calldata signature
     ) external view override returns (ValidationData) {
-        if (allowedRequestor[permissionId][caller][kernel]) {
+        if (allowedRequestor[msg.sender][permissionId][caller][kernel]) {
             return ValidationData.wrap(0);
         }
         return SIG_VALIDATION_FAILED;
