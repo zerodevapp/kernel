9a81b95eac2630a63333aed16c63becf0658de65
leekt <leekt216@gmail.com>
2025-08-09 02:26:37 +0900

    check

--- src/KernelFactory.sol
+++ src/KernelFactory.sol
@@ -15,6 +15,25 @@ contract KernelFactory {
         immutableECDSA = _immutableECDSA;
     }
 
+    function checkInitialized(address account, bytes calldata initData) external view returns (bool) {
+        bytes4 selector = bytes4(initData[0:4]);
+        ( /*replayable*/ , /*nonce*/, Install[] memory packages, /*sig*/ ) =
+            abi.decode(initData, (bool, uint256, Install[], bytes));
+
+        // naively check if the package has been installed by only checking the nonce
+        for (uint256 i = 0; i < packages.length; i++) {
+            Install memory p = packages[i];
+            uint256 n = p.nonce;
+            uint192 key = uint192(p.nonce >> 8);
+            uint64 seq = uint64(p.nonce);
+
+            if (Kernel(account).nonce(key) <= seq) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     // Kernel UUPS
     function deploy(Install[] calldata initialPackages, uint256 nonce) external payable returns (Kernel) {
         bytes32 salt = keccak256(abi.encode(initialPackages, nonce));
