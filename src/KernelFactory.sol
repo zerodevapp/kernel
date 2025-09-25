e850e7aa95f9d82c8a05d888aa6fa966089b6453
leekt <leekt216@gmail.com>
2025-09-25 14:59:41 +0900

    fix: signer cannot be address(0)

--- src/KernelFactory.sol
+++ src/KernelFactory.sol
@@ -75,6 +75,7 @@ contract KernelFactory {
         payable
         returns (Kernel)
     {
+        require(signer != address(0), InvalidSigner());
         bytes32 salt = keccak256(abi.encode(initialPackages, nonce));
         (, address account) =
             LibClone.createDeterministicERC1967(address(IMMUTABLE_ECDSA), abi.encodePacked(signer), salt);
@@ -90,6 +91,7 @@ contract KernelFactory {
         uint256 nonce,
         bytes calldata extraCall
     ) external payable returns (Kernel) {
+        require(signer != address(0), InvalidSigner());
         bytes32 salt = keccak256(abi.encode(initialPackages, nonce));
         (, address account) =
             LibClone.createDeterministicERC1967(address(IMMUTABLE_ECDSA), abi.encodePacked(signer), salt);
