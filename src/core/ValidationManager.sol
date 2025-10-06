7750c812e8aa96254dbd9e952509a017f389e337
leekt <leekt216@gmail.com>
2025-10-06 22:34:48 +0900

    fix: remove validationId check from setRoot

--- src/core/ValidationManager.sol
+++ src/core/ValidationManager.sol
@@ -283,7 +283,6 @@ abstract contract ValidationManager {
     }
 
     function _setRoot(ValidationId vId) internal {
-        require(ValidationId.unwrap(vId) != bytes20(0), InvalidRootValidation());
         ValidationStorage storage $ = _validationStorage();
         $.root = vId;
     }
