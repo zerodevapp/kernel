25e51e1f0a1d84a49296c0fce6c1f9efe328c2a1
leekt <leekt216@gmail.com>
2025-10-22 02:17:13 +0900

    removed checkInitialized

--- src/KernelFactory.sol
+++ src/KernelFactory.sol
@@ -17,30 +17,6 @@ contract KernelFactory {
         IMMUTABLE_ECDSA = _immutableEcdsa;
     }
 
-    function checkInitialized(address account, bytes calldata initData) external view returns (bool) {
-        (
-
-            /*replayable*/,
-            /*nonce*/,
-            Install[] memory packages, /*sig*/
-        ) = abi.decode(initData, (bool, uint256, Install[], bytes));
-
-        // naively check if the package has been installed, does not guarantee if proper internalData is used
-        for (uint256 i = 0; i < packages.length; i++) {
-            Install memory p = packages[i];
-
-            bytes memory context;
-            if (p.moduleType == 5 || p.moduleType == 6) {
-                context = abi.encodePacked(bytes20(p.internalData));
-            }
-
-            if (!Kernel(payable(account)).isModuleInstalled(p.moduleType, p.module, context)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     // Kernel UUPS
     function deploy(Install[] calldata initialPackages, uint256 nonce) external payable returns (Kernel) {
         bytes32 salt = _calculateSalt(initialPackages, nonce);
